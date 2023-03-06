e3a100d6bb296d490a1f83e574ae218f216738a3
HaroldSP <george.st.petersburg@gmail.com>
2023-03-06 17:44:14 +0300

    added bg

--- src/modules/one.js
+++ src/modules/one.js
@@ -6,7 +6,38 @@
 
 const oneModule = function () {
   console.log('one.js');
-//   console.log('test');
+  let dataRecievied;
+
+  const getData = (source = 'dbHeroes.json') => {
+    return fetch(source)
+      .then(response => {
+        if (!response.ok) { console.log('data not loaded'); return }
+        return response.json();
+      })
+      .then(data => {
+        // console.log(data);
+        // other data manipulations
+        return data;
+      })
+      .catch(error => console.error('An error occurred:', error));
+  };
+
+  getData()
+    .then(data => {
+      // console.log(data, typeof data);
+      let allKeysArr = [];
+
+      for (const obj of data) {
+        // console.log(obj);
+        for (const key in obj) {
+          // const value = obj[key];
+          // console.log(key, typeof key);
+
+          if (!allKeysArr.includes(key)) allKeysArr.push(key);
+        }
+      }
+      console.log(allKeysArr, typeof data);
+    });
 }
 
 export default oneModule;
